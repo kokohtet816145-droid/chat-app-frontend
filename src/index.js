f885ea0e0f4931dfd8c82285f040a424982501e7
kokohtet816145-droid <kokohtet816145@gmail.com>
2026-04-16 05:45:56 +0630

    Add vConsole for debugging blank screen

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import VConsole from 'vconsole';
+
+const vConsole = new VConsole();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
