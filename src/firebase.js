22681a7cda374a320a6c0a4e31097d054b9e6a0b
kokohtet816145-droid <kokohtet816145@gmail.com>
2026-04-17 12:32:16 +0630

    Step 2: Add Firebase config only

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDkaa-a8xD0-mCT_gnszSHmTf_ZSmLzQ3k",
@@ -13,4 +12,3 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
